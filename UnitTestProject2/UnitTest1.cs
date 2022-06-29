f6cdb02c82433193f8851a45e51d132ee6c38de8
jonaslagoni <jonas-lt@live.dk>
2022-06-29 22:12:52 +0200

    test: add test project

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Oxide.Plugins;
+using System.Reflection;
+using System.Collections.Generic;
+using ConVar;
+using Oxide.Ext.GamingApi.MessageQueue;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GamingAPI plugin = new GamingAPI();
+            MethodInfo methodInfo = typeof(GamingAPI).GetMethod("OnPlayerChat", BindingFlags.NonPublic | BindingFlags.Instance);
+            var p = new BasePlayer();
+            p.UserIDString = "test";
+            p.displayName = "test"; 
+            object[] parameters = { p, "test", Chat.ChatChannel.Global };
+            methodInfo.Invoke(plugin, parameters);
+            Assert.AreEqual(GamingApiMessageQueue.Instance.GetCurrentMessagesInQueue(), 1);
+        }
+    }
+}
