f8b96d3c6c9213e497f1beb68ca1f6d9671662fb
jonaslagoni <jonas-lt@live.dk>
2022-10-16 19:48:58 +0200

    feat: update dependency and add tests

---
+++ GamingAPIPluginsTest/ServerStartedTest.cs
@@ -0,0 +1,15 @@
+
+using System.Text.Json;
+using Asyncapi.Nats.Client.Models;
+using Xunit;
+
+namespace Asyncapi.Nats.Client.Tests
+{
+    public class ServerStartedTest
+    {
+        [Fact]
+        public void ShouldSerializeAndDeserializeAccurately()
+        {
+        }
+    }
+}
